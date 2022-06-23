c80443a4f2fb9b927291ba37e8875fc6fe88dc8c
MSDNicrosoft <wang3311835119@hotmail.com>
2022-06-23 17:47:12 +0800

    Initial commit - Support 1.18.2

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chat Up!
+Raises in game chat by 20 pixels to improve visibility of armour hunches 
---
+++ src/main/java/chatup/mixins/ChatUpMixin.java
@@ -0,0 +1,45 @@
+package chatup.mixins;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ChatHud.class)
+public class ChatUpMixin {
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    private int getOffset() {
+        ClientPlayerEntity player = this.client.player;
+        if (player == null || player.isCreative() || player.isSpectator()) return 0;
+        int offset = player.getArmor() > 0 ? 20 : 0;
+        if (player.getAbsorptionAmount() > 0) offset += 20;
+        return offset;
+    }
+
+    @ModifyArg(
+            method = "render",
+            index = 1,
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/util/math/MatrixStack;translate(DDD)V",
+                    ordinal = 0
+            )
+    )
+    private double offsetY(double y) {
+        return y - getOffset();
+    }
+
+    @ModifyConstant(method = "getText", constant = @Constant(doubleValue = 80.0))
+    private double textBottomOffset(double original) {
+        return original + getOffset();
+    }
+}
